fefddfe3e87ec55e42d55068ce839a01c49fdbe7
Jared1407 <jdguthri@asu.edu>
2025-11-10 20:17:59 -0700

    Completed up to parameter update with batch gradient.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <random>
 #include <numeric>
+#include <cstdlib>
+#include <map>
 using Eigen::MatrixXd;
 
 //---------------- Data Structures ----------------//
@@ -427,8 +429,6 @@ PredictionResult model_predict(ModelParameters params, const Eigen::MatrixXd& X,
 }
 
 // Begin setup for Multi-Category Neural Net:
-
-std::pair<Eigen::MatrixXd, Eigen::MatrixXd> relu(const Eigen::MatrixXd& Z){
     /*
     Computes relu activation of input Z
     
@@ -439,7 +439,7 @@ std::pair<Eigen::MatrixXd, Eigen::MatrixXd> relu(const Eigen::MatrixXd& Z){
         A: where A = ReLU(Z) is a numpy.ndarray (n, m) representing 'm' samples each of 'n' dimension
         cache: a dictionary with {"Z", Z}
     */
-    
+std::pair<Eigen::MatrixXd, Eigen::MatrixXd> relu(const Eigen::MatrixXd& Z){
     Eigen::MatrixXd A = Z.cwiseMax(0.0);
     Eigen::MatrixXd cache;
     cache = Z;
@@ -447,7 +447,6 @@ std::pair<Eigen::MatrixXd, Eigen::MatrixXd> relu(const Eigen::MatrixXd& Z){
 }
 
 // ReLU Gradient:
-Eigen::MatrixXd relu_der(const Eigen::MatrixXd& dA, const Eigen::MatrixXd& cache){
     /*
     Computes derivative of relu activation
     
@@ -461,6 +460,7 @@ Eigen::MatrixXd relu_der(const Eigen::MatrixXd& dA, const Eigen::MatrixXd& cache
         dZ: the derivative of dimension (n,m). It is the elementwise 
             product of the derivative of ReLU and dA
     */
+Eigen::MatrixXd relu_der(const Eigen::MatrixXd& dA, const Eigen::MatrixXd& cache){
     Eigen::MatrixXd Z = cache;
     Eigen::MatrixXd dZ = dA;
     dZ = dZ.array() * (Z.array() > 0).cast<double>();
@@ -468,7 +468,6 @@ Eigen::MatrixXd relu_der(const Eigen::MatrixXd& dA, const Eigen::MatrixXd& cache
 }
 
 // Linear Activation: Linear(Z) = Z
-std::pair<Eigen::MatrixXd, Eigen::MatrixXd> linear(const Eigen::MatrixXd& Z){
     /*
     Computes linear activation of Z
     This function is implemented for completeness
@@ -480,13 +479,13 @@ std::pair<Eigen::MatrixXd, Eigen::MatrixXd> linear(const Eigen::MatrixXd& Z){
         A: where A = Linear(Z) is a numpy.ndarray (n, m) representing 'm' samples each of 'n' dimension
         cache: a dictionary with {"Z", Z}  
     */
+std::pair<Eigen::MatrixXd, Eigen::MatrixXd> linear(const Eigen::MatrixXd& Z){
     Eigen::MatrixXd A = Z;
     Eigen::MatrixXd cache = Z;
     return {A, cache};
 }
 
 // Linear Activation Gradient:
-Eigen::MatrixXd linear_der(const Eigen::MatrixXd& dA,const Eigen::MatrixXd& cache){
     /*
     Computes derivative of linear activation
     This function is implemented for completeness
@@ -501,17 +500,13 @@ Eigen::MatrixXd linear_der(const Eigen::MatrixXd& dA,const Eigen::MatrixXd& cach
         dZ: the derivative of dimension (n,m). It is the elementwise 
             product of the derivative of Linear(.) and dA
     */
-
+Eigen::MatrixXd linear_der(const Eigen::MatrixXd& dA,const Eigen::MatrixXd& cache){
     Eigen::MatrixXd dZ = dA;
     return dZ;
 }
 
 
 //Softmax Cross Entropy Loss:
-
-
-
-SoftCrossEntropyLoss softmax_cross_entropy_loss(const Eigen::MatrixXd& Z, const Eigen::MatrixXd& Y = Eigen::MatrixXd()){
     /*
     Computes the softmax activation of the inputs Z
     Estimates the cross entropy loss
@@ -526,8 +521,7 @@ SoftCrossEntropyLoss softmax_cross_entropy_loss(const Eigen::MatrixXd& Z, const 
         cache: a dictionary to store the activations which will be used later to estimate derivatives
         loss: cost of prediction
     */
-    
-    
+SoftCrossEntropyLoss softmax_cross_entropy_loss(const Eigen::MatrixXd& Z, const Eigen::MatrixXd& Y = Eigen::MatrixXd()){
     long n = Z.rows(); // number of classes
     long m = Z.cols(); // number of samples
     Eigen::MatrixXd exp_Z = Z.array().exp();
@@ -548,8 +542,7 @@ SoftCrossEntropyLoss softmax_cross_entropy_loss(const Eigen::MatrixXd& Z, const 
     return result;
 }
 
-
-Eigen::MatrixXd softmax_cross_entropy_loss_der(const Eigen::MatrixXd& Y, const Eigen::MatrixXd& cache){
+// Softmax Cross Entropy Loss Derivative:
     /*
     Computes the derivative of the softmax activation and cross entropy loss
 
@@ -560,6 +553,7 @@ Eigen::MatrixXd softmax_cross_entropy_loss_der(const Eigen::MatrixXd& Y, const E
     Outputs:
         dZ: derivative dL/dZ - a numpy.ndarray of dimensions (n, m) 
     */
+Eigen::MatrixXd softmax_cross_entropy_loss_der(const Eigen::MatrixXd& Y, const Eigen::MatrixXd& cache){
     Eigen::MatrixXd A = cache;
     long m = Y.cols(); // number of samples
     Eigen::MatrixXd dZ = (A - Y) / m;
@@ -572,7 +566,6 @@ struct parameters {
 };
 
 // Init multi-layer NN:
-parameters initialize_network(const std::vector<int>& net_dims) {
     /*
     Initializes the parameters of a multi-layer neural network
     
@@ -583,7 +576,16 @@ parameters initialize_network(const std::vector<int>& net_dims) {
     
     Outputs:
         parameters: Python Dictionary for storing the Weights and bias of each layer of the network
+
+    numLayers = len(net_dims)
+    parameters = {}
+    for l in range(numLayers-1):
+        parameters["W"+str(l+1)] = 0.01 * np.random.randn(net_dims[l + 1],net_dims[l])
+        parameters["b"+str(l+1)] = np.zeros((net_dims[l + 1],1))
+
+    return parameters
     */
+parameters initialize_network(const std::vector<int>& net_dims) {
     std::mt19937 gen(42);
     
 
@@ -601,8 +603,7 @@ parameters initialize_network(const std::vector<int>& net_dims) {
 
     return params;
 }
-
-std::pair<Eigen::MatrixXd, Eigen::MatrixXd> linear_forward(const Eigen::MatrixXd &A_prev, const parameters &params){
+// Linear Forward: 
     /*
     Input A_prev propagates through the layer 
     Z = WA + b is the output of this layer. 
@@ -615,43 +616,390 @@ std::pair<Eigen::MatrixXd, Eigen::MatrixXd> linear_forward(const Eigen::MatrixXd
     Outputs:
         Z: where Z = W.A_prev + b, where Z is the numpy.ndarray (n_out, m) dimensions
         cache: a dictionary containing the inputs A
-    */
 
+
+    cache = {"A": A_prev}
+
+    Z = np.dot(W,A_prev) + b
+
+    return Z, cache
+    
+    */
+std::pair<Eigen::MatrixXd, Eigen::MatrixXd> linear_forward(const Eigen::MatrixXd &A_prev, const parameters &params){
     Eigen::MatrixXd cache  = A_prev;
 
-    Eigen::MatrixXd Z = params.W * A_prev;
-    Z.colwise() += params.b;
+    Eigen::MatrixXd Z = (params.W * A_prev).colwise() + params.b.col(0);
 
     return {Z, A_prev};
 }
 
+// Layer Forward:
+ /*
+    Input A_prev propagates through the layer and the activation
+
+    Inputs: 
+        A_prev: numpy.ndarray (n,m) the input to the layer
+        W: numpy.ndarray (n_out, n) the weights of the layer
+        b: numpy.ndarray (n_out, 1) the bias of the layer
+        activation: is the string that specifies the activation function
+
+    Outputs:
+        A: = g(Z), where Z = WA + b, where Z is the numpy.ndarray (n_out, m) dimensions
+        g is the activation function
+        cache: a dictionary containing the cache from the linear and the nonlinear propagation
+        to be used for derivative
+        
+    Z, lin_cache = linear_forward(A_prev, W, b)
+    if activation == "relu":
+        A, act_cache = relu(Z)
+    elif activation == "linear":
+        A, act_cache = linear(Z)
+
+    cache = {}
+    cache["lin_cache"] = lin_cache
+    cache["act_cache"] = act_cache
+    return A, cache
+        
+    */
+
+std::pair<Eigen::MatrixXd, Eigen::MatrixXd> layer_forward(const Eigen::MatrixXd &A_prev, const parameters &params, const std::string &activation){
+    auto linearForward = linear_forward(A_prev, params);
+    Eigen::MatrixXd Z = linearForward.first;
+    Eigen::MatrixXd lin_cache = linearForward.second;
+
+    Eigen::MatrixXd A;
+    Eigen::MatrixXd act_cache;
+
+    if(activation == "relu"){
+        auto reluForward = relu(Z);
+        A = reluForward.first;
+        act_cache = reluForward.second;
+        return {A, lin_cache};
+    } else if(activation == "linear"){
+        auto linearForwardAct = linear(Z);
+        A = linearForwardAct.first;
+        act_cache = linearForwardAct.second;
+        return {A, lin_cache};
+    } else {
+        throw std::invalid_argument("Unsupported activation function");
+    }
+
+    Eigen::MatrixXd cache = lin_cache + act_cache;
+    
+
+    return {A, cache};
+}
+
+// Multi-Layer Forward:
+    /*
+    Forward propgation through the layers of the network
+
+    Inputs: 
+        A0: numpy.ndarray (n,m) with n features and m samples
+        parameters: dictionary of network parameters {"W1":[..],"b1":[..],"W2":[..],"b2":[..]...}
+    
+    Outputs:
+        AL: numpy.ndarray (c,m)  - outputs of the last fully connected layer before softmax
+            where c is number of categories and m is number of samples
+        caches: a dictionary of associated caches of parameters and network inputs
+
+    L = len(parameters)//2  
+    A = A0
+    caches = []
+    
+    for l in range(1,L):
+        A, cache = layer_forward(A, parameters["W"+str(l)], parameters["b"+str(l)], "relu")
+        caches.append(cache)
+    
+    AL, cache = layer_forward(A, parameters["W"+str(L)], parameters["b"+str(L)], "linear")
+    caches.append(cache)
+    return AL, caches
+    */
+std::pair<Eigen::MatrixXd, std::vector<Eigen::MatrixXd>> multi_layer_forward(const Eigen::MatrixXd &A0, const parameters &params){
+    int L = params.W.rows(); // Number of layers
+    Eigen::MatrixXd A = A0;
+    std::vector<Eigen::MatrixXd> caches;
+
+    for(int l = 1; l < L; l++){
+        auto layerForward = layer_forward(A, params, "relu");
+        A = layerForward.first;
+        Eigen::MatrixXd cache = layerForward.second;
+        caches.push_back(cache);
+    }
+    auto layerForward = layer_forward(A, params, "linear");
+    Eigen::MatrixXd AL = layerForward.first;
+    caches.push_back(layerForward.second);
+
+    return {AL, caches};
+}
+
+// Backward Propagation Through a Single Layer:
+/*
+'''
+    Backward prpagation through the linear layer
+
+    Inputs:
+        dZ: numpy.ndarray (n,m) derivative dL/dz 
+        cache: a dictionary containing the inputs A, for the linear layer
+            where Z = WA + b,    
+            Z is (n,m); W is (n,p); A is (p,m); b is (n,1)
+        W: numpy.ndarray (n,p)
+        b: numpy.ndarray (n, 1)
+
+    Outputs:
+        dA_prev: numpy.ndarray (p,m) the derivative to the previous layer
+        dW: numpy.ndarray (n,p) the gradient of W 
+        db: numpy.ndarray (n, 1) the gradient of b
+    '''
+    A = cache["A"]
+    m = dZ.shape[1]
+    
+    # Compute dA_prev
+    dA_prev = np.dot(W.T, dZ)
+    
+    # Compute dW
+    dW = np.dot(dZ, A.T)
+    
+    # Compute db (sum across the rows)
+    db = np.sum(dZ, axis=1, keepdims=True)
+
+    
+    
+    return dA_prev, dW, db
+*/
+struct BackwardResult {
+    Eigen::MatrixXd dA_prev;
+    Eigen::MatrixXd dW;
+    Eigen::MatrixXd db;
+};
+BackwardResult linear_backward(const Eigen::MatrixXd& dZ, const Eigen::MatrixXd& cache, const parameters& params){
+    Eigen::MatrixXd A = cache;
+
+    // Compute dA_prev
+    Eigen::MatrixXd dA_prev = params.W.transpose() * dZ;
+
+    // Compute dW
+    Eigen::MatrixXd dW = (dZ * A.transpose());
+
+    // Compute db
+    Eigen::MatrixXd db = dZ.rowwise().sum();
+
+    BackwardResult result;
+    result.dA_prev = dA_prev;
+    result.dW = dW;
+    result.db = db;
+
+    return result;
+}
+
+// Back Propagation with Activation:
+/*
+    '''
+    Backward propagation through the activation and linear layer
+
+    Inputs:
+        dA: numpy.ndarray (n,m) the derivative to the previous layer
+        cache: dictionary containing the linear_cache and the activation_cache
+        activation - activation of the layer
+        W: numpy.ndarray (n,p)
+        b: numpy.ndarray (n, 1)
+    
+    Outputs:
+        dA_prev: numpy.ndarray (p,m) the derivative to the previous layer
+        dW: numpy.ndarray (n,p) the gradient of W 
+        db: numpy.ndarray (n, 1) the gradient of b
+    '''
+
+    lin_cache = cache["lin_cache"]
+    act_cache = cache["act_cache"]
+
+    if activation == "relu":
+        dZ = relu_der(dA, act_cache)
+    elif activation == "linear":
+        dZ = linear_der(dA, act_cache)
+    dA_prev, dW, db = linear_backward(dZ, lin_cache, W, b)
+    return dA_prev, dW, db
+*/
+
+BackwardResult layer_backward(const Eigen::MatrixXd& dA, const Eigen::MatrixXd& cache, const parameters& params, const std::string& activation){
+    // Seperate lin_cache and act_cache from cache
+
+    Eigen::MatrixXd lin_cache = cache; 
+    Eigen::MatrixXd act_cache = cache; 
+
+    Eigen::MatrixXd dZ;
+    if(activation == "relu"){
+        dZ = relu_der(dA, act_cache);
+    } else if(activation == "linear"){
+        dZ = linear_der(dA, act_cache);
+    } else {
+        throw std::invalid_argument("Unsupported activation function");
+    }
+
+    BackwardResult backResult = linear_backward(dZ, lin_cache, params);
+
+    return backResult;
+}
+
+
+// Multi-Layer Backward:
+/*
+'''
+    Back propgation through the layers of the network (except softmax cross entropy)
+    softmax_cross_entropy can be handled separately
+
+    Inputs: 
+        dAL: numpy.ndarray (n,m) derivatives from the softmax_cross_entropy layer
+        caches: a dictionary of associated caches of parameters and network inputs
+        parameters - dictionary of network parameters {"W1":[..],"b1":[..],"W2":[..],"b2":[..]...}
+
+    Outputs:
+        gradients: dictionary of gradient of network parameters 
+            {"dW1":[..],"db1":[..],"dW2":[..],"db2":[..],...}
+    '''
+
+    L = len(caches) 
+    gradients = {}
+    dA = dAL
+    activation = "linear"
+    for l in reversed(range(1,L+1)):
+        dA, gradients["dW"+str(l)], gradients["db"+str(l)] = \
+                    layer_backward(dA, caches[l-1], \
+                    parameters["W"+str(l)],parameters["b"+str(l)],\
+                    activation)
+        activation = "relu"
+    return gradients
+*/
+
+std::map<std::string, Eigen::MatrixXd> multi_layer_backward(const Eigen::MatrixXd& dAL, const std::vector<Eigen::MatrixXd>& caches, const parameters& params){
+    int L = caches.size();
+    std::map<std::string, Eigen::MatrixXd> gradients;
+    Eigen::MatrixXd dA = dAL;
+    std::string activation = "linear";
+
+    for(int l = L; l >= 1; l--){
+        BackwardResult backResult = layer_backward(dA, caches[l - 1], params, activation);
+        dA = backResult.dA_prev;
+        gradients["dW" + std::to_string(l)] = backResult.dW;
+        gradients["db" + std::to_string(l)] = backResult.db;
+        activation = "relu";
+    }
+
+    return gradients;
+}
+
+// Prediction:
+/*
+'''
+    Network prediction for inputs X
+
+    Inputs: 
+        X: numpy.ndarray (n,m) with n features and m samples
+        parameters: dictionary of network parameters 
+            {"W1":[..],"b1":[..],"W2":[..],"b2":[..],...}
+    Outputs:
+        YPred: numpy.ndarray (1,m) of predictions
+    '''
+    # Forward propagate input 'X' using multi_layer_forward(.) and obtain the final activation 'A'
+    # Using 'softmax_cross_entropy loss(.)', obtain softmax activation 'AL' with input 'A' from step 1
+    # Predict class label 'YPred' as the 'argmax' of softmax activation from step-2. 
+    # Note: the shape of 'YPred' is (1,m), where m is the number of samples
+
+    # your code here
+    A, cache = multi_layer_forward(X, parameters)
+    AL, cache, loss = softmax_cross_entropy_loss(A)
+
+    YPred = np.argmax(AL, axis=0, keepdims = True)
+  
+
+    return YPred
+*/
+
+std::vector<double> classify(const Eigen::MatrixXd& X, const parameters& params){
+    auto forwardResult = multi_layer_forward(X, params);
+    Eigen::MatrixXd A = forwardResult.first;
+
+    SoftCrossEntropyLoss softmaxResult = softmax_cross_entropy_loss(A);
+    Eigen::MatrixXd AL = softmaxResult.A;
+
+    std::vector<double> YPred(AL.cols());
+    for(int i = 0; i < AL.cols(); i++){
+        AL.col(i).maxCoeff(&YPred[i]);
+    }
+
+    return YPred;
+}
+
+
+// Parameter Update using Batch-Gradient:
+/*
+'''
+    Updates the network parameters with gradient descent
+
+    Inputs:
+        parameters: dictionary of network parameters 
+            {"W1":[..],"b1":[..],"W2":[..],"b2":[..],...}
+        gradients: dictionary of gradient of network parameters 
+            {"dW1":[..],"db1":[..],"dW2":[..],"db2":[..],...}
+        epoch: epoch number
+        alpha: step size or learning rate
+        
+    Outputs:
+        parameters: updated dictionary of network parameters 
+            {"W1":[..],"b1":[..],"W2":[..],"b2":[..],...}
+    '''
+    
+    L = len(parameters)//2
+    for i in range(L):
+        
+        parameters["W"+str(i+1)] -= alpha * gradients["dW" + str(i+1)]
+        parameters["b"+str(i+1)] -= alpha * gradients["db" + str(i+1)]
+
+    return parameters
+*/
+
+std::map<std::string, Eigen::MatrixXd> update_parameters(const std::map<std::string, Eigen::MatrixXd>& parameters, const std::map<std::string, Eigen::MatrixXd>& gradients, double epoch, double alpha){
+    int L = parameters.size() / 2;
+    std::map<std::string, Eigen::MatrixXd> updated_params = parameters;
+
+    for(int i = 0; i < L; i++){
+        updated_params["W" + std::to_string(i + 1)] -= alpha * gradients.at("dW" + std::to_string(i + 1));
+        updated_params["b" + std::to_string(i + 1)] -= alpha * gradients.at("db" + std::to_string(i + 1));
+    }
+
+    return updated_params;
+}
+
+
+
+
 
 
 int main(int argc, char *argv[]) {
+    // __________TODO___________ //
+    // Convert all caches to maps
+
+
     // --- Setup hyperparameters and data dimensions ---
     std::vector<int> test_init;
     for(int i = 1; i < argc; i ++){
         test_init.push_back(atoi(argv[i]));
     }
 
-    parameters parameterTest = initialize_network(test_init);
 
-
+    // Test Linear Forward:
     int n1 = 3;
     int m1 = 4;
-    Eigen::MatrixXd A_prev_t = Eigen::MatrixXd::Random(n1,m1);
-    Eigen::MatrixXd W_t = Eigen::MatrixXd::Random(n1,n1);
-    Eigen::MatrixXd b_t = Eigen::MatrixXd::Random(n1,1);
-    parameters superTest;
-    superTest.b = b_t;
-    superTest.W = W_t;
-
-    
-    std::pair<Eigen::MatrixXd, Eigen::MatrixXd> Z_est, cache_est = linear_forward(A_prev_t, superTest);
-
-    // std::cout << parameterTest.W << std::endl;
-    // std::cout << parameterTest.b << std::endl;
-
+    int p1 = 5;
+    Eigen::MatrixXd dZ_t = Eigen::MatrixXd::Random(n1, m1);
+    Eigen::MatrixXd A_t = Eigen::MatrixXd::Random(p1, m1);
+    parameters params_t;
+    params_t.W = Eigen::MatrixXd::Random(n1, p1);
+    params_t.b = Eigen::MatrixXd::Random(n1, 1);
+    BackwardResult backTest = linear_backward(dZ_t, A_t, params_t);
+    std::cout << "Backward Test dA_prev: " << "\n" << backTest.dA_prev << std::endl;
+    std::cout << "Backward Test dW: " << "\n" << backTest.dW << std::endl;
+    std::cout << "Backward Test db: " << "\n" << backTest.db << std::endl;
 
     return 0;
 }
